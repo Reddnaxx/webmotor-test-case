47c0774f0445faed60a416500a31740aa0024849
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-10-18 15:58:32 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: 'export',
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
-
-  assetPrefix: './',
-};
+const nextConfig = {};
 
 export default nextConfig;
